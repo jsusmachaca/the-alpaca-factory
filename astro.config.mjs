7e5fb7eabdbe274de38b929e4eb42761b5a261b3
jsusmachaca <falcorgd@gmail.com>
2024-03-02 23:00:58 -0500

    add adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
+import awsAmplify from "astro-aws-amplify"
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'hybrid'
+  output: 'hybrid',
+  adapter: awsAmplify()
 });
