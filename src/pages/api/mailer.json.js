6d9d3cbb853c8791b7995e85b4841e1314bd2ffe
jsusmachaca <falcorgd@gmail.com>
2024-03-12 10:54:58 -0500

    update gallery

--- src/pages/api/mailer.json.js
+++ src/pages/api/mailer.json.js
@@ -14,50 +14,51 @@ export async function POST({ redirect, request }) {
   const message = data.get("message");
   const phone = data.get("phone")
 
-  const msg = {
-    to: addressee,
-    from: {
-      name: name,
-      email: addressee,
-    },
-    subject: 'Consulta/Contacto desde The Alpaca Factory.',
-    html: `
-      <div style=" padding-top: 5px; padding-bottom: 5px; background-color: #b8b8b8; border-radius: 20px; ">
-        <div style=" text-align: center;">
-          <h1 style="font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;">Hola</h1> 
-          <h1 style="font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;">The Alpaca Factory</h1> 
-        </div>
-        <div style="display: flex; justify-content: center; margin: 20px;">
-          <p style="font-family:'Courier New', Courier, monospace; font-weight: bold;">${message}</p>
-        </div>
+  try {
+    const msg = {
+      to: addressee,
+      from: {
+        name: name,
+        email: addressee,
+      },
+      subject: 'Consulta/Contacto desde The Alpaca Factory.',
+      html: `
+        <div style=" padding-top: 5px; padding-bottom: 5px; background-color: #b8b8b8; border-radius: 20px; ">
+          <div style=" text-align: center;">
+            <h1 style="font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;">Hola</h1> 
+            <h1 style="font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;">The Alpaca Factory</h1> 
+          </div>
+          <div style="display: flex; justify-content: center; margin: 20px;">
+            <p style="font-family:'Courier New', Courier, monospace; font-weight: bold;">${message}</p>
+          </div>
 
-        <div style="margin: 10px 20px; background-color: #f9f9ff; padding: 5px; border-radius: 20px;">
-          <div style="display: flex; align-items: center; margin-left: 10px;">
-            <p style="font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;">
-                Nombre: <span style="font-weight: bold;">${name}</span> 
-            </p>
-          </div>
-          <div style="display: flex; align-items: center; margin-left: 10px;">
-            <p style="font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;">
-                Número: <span style="font-weight: bold;">${phone}</span> 
-            </p>
-          </div>
-          <div style="display: flex; align-items: center; margin-left: 10px;">
+          <div style="margin: 10px 20px; background-color: #f9f9ff; padding: 5px; border-radius: 20px;">
+            <div style="display: flex; align-items: center; margin-left: 10px;">
               <p style="font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;">
-                  Correo electrónico: <span style="font-weight: bold;">${email}</span> 
+                  Nombre: <span style="font-weight: bold;">${name}</span> 
               </p>
+            </div>
+            <div style="display: flex; align-items: center; margin-left: 10px;">
+              <p style="font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;">
+                  Número: <span style="font-weight: bold;">${phone}</span> 
+              </p>
+            </div>
+            <div style="display: flex; align-items: center; margin-left: 10px;">
+                <p style="font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;">
+                    Correo electrónico: <span style="font-weight: bold;">${email}</span> 
+                </p>
+            </div>
           </div>
         </div>
-      </div>
-    `
-  }
-  sgMail.send(msg)
-    .then(res => {
-      console.log("Mail Send")
-    })
-    .catch(err => {
-      console.error(err)
-    })
+      `
+    }
 
-  return redirect("/sending", 307)
+    const sending = await sgMail.send(msg)
+    console.log(sending)
+    return redirect("/sending", 307)
+
+  } catch (error) {
+    console.error(error)
+    return redirect("/error", 307)
+  }
 }
