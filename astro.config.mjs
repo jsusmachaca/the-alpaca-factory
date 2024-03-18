76038470510e8f9e8fbea68cb71454808292e7a4
jsusmachaca <falcorgd@gmail.com>
2024-03-18 15:06:30 -0500

    add adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,4 +10,7 @@ export default defineConfig({
   },
   integrations: [tailwind()],
   output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
