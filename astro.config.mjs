8550a108c737ce6f5c3f99a9f0fc4bbef02203f6
jsusmachaca <falcorgd@gmail.com>
2024-03-22 14:36:35 -0500

    update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,7 @@ import node from "@astrojs/node";
 // https://astro.build/config
 export default defineConfig({
   server: {
-    host: "0.0.0.0",
-    https: true,
+    host: "0.0.0.0"
   },
   integrations: [tailwind()],
   output: "server",
